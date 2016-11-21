b78c40737f06d71999e186cddf1ffc4f4625dd96
José I. Crespo-Anadón <jcrespo@nevis.columbia.edu>
2016-11-20 23:16:56 -0500

    Increase plotter speed
    Replace TTree:Draw with C++

--- make.sh
+++ make.sh
@@ -4,5 +4,4 @@ rootcint -f decoder_dict.cc -c decoder.hh HeaderInfo.hh LinkDef.h
 echo "Compiling decoder.cc\n"
 g++ decoder_dict.cc decoder.cc -Wall -o decoder.exe `root-config --cflags  --glibs`
 echo "Compiling plotter.cc\n"
-g++ plotter.cc -Wall -o plotter.exe `root-config --cflags  --glibs`
-
+g++ decoder_dict.cc plotter.cc -Wall -o plotter.exe `root-config --cflags  --glibs`
--- plotter.cc
+++ plotter.cc
@@ -15,6 +15,9 @@
 int plotter( const char* argv ){
   gStyle->SetOptTitle(1); // Title in canvas
   gStyle->SetLineWidth(1); // Thinnest lines
+  gStyle->SetTitleOffset(1.2*(gStyle->GetTitleOffset("y")), "y"); // Add 20% offset to the vertical title
+  gStyle->SetPadRightMargin(0.75*(gStyle->GetPadRightMargin()));
+  gStyle->SetPadLeftMargin(1.17*(gStyle->GetPadLeftMargin()));
 
   TFile inFile( argv, "READ" );
   if( !inFile.IsOpen() ){
@@ -31,6 +34,9 @@ int plotter( const char* argv ){
     exit(0);
   }
   else std::cout << "Tree found: " << inTreeName << std::endl;
+
+  std::vector< std::vector<uint16_t> >* waveform = NULL;
+  inTree->SetBranchAddress("waveform", &waveform);
 
   // Canvas showing 64 channels
   TCanvas* canvas64 = new TCanvas("wfdisplay64", "Waveform display: 64 channels", 1024, 700);
@@ -50,9 +56,8 @@ int plotter( const char* argv ){
   std::string prompt;
   int event = 1;
   int maxEvent = inTree->GetEntries();
-  char cmd1[512], cmd2[512]; // Buffers for commands in TTree::Draw
   int chPad64 = 64/(colPad64 * rowPad64); // Number of channels per pad
-  int chPad16 = 64/(int(vcanvas16.size()) * colPad16 * rowPad16); // Number of channels per pad
+  // int chPad16 = 64/(int(vcanvas16.size()) * colPad16 * rowPad16); // Number of channels per pad
   std::vector<TMultiGraph*> vmgr(colPad64 * rowPad64); // Vector of pointers for memory management
   //  std::vector<TGraph*> vgr2(int(vcanvas16.size()) * colPad16 * rowPad16);
   std::vector<TGraph*> vgr2; // Vector of pointers for memory management
@@ -60,49 +65,46 @@ int plotter( const char* argv ){
   // For a more generic approach, TMultiGraphs should be used
 
   while( event >= 1 && event <= maxEvent ){
-    sprintf(cmd2, "Entry$==%i", event - 1); // FEM starts counting events from 1 but TTree starts counting entries from 0
+    inTree->GetEntry(event - 1);
     for(int ich = 0; ich < 64; ich++){
-      // All channels in same canvas
-      canvas64->cd( (int)floor(ich/chPad64) + 1 ); // Plot "chPad64" channels per pad
       TMultiGraph*& mgr = vmgr.at( (size_t)floor(ich/chPad64) ); // Reference pointer
-      sprintf(cmd1, "waveform[%i][] + 100.*%i:Iteration$", ich, ich); // Add offset (100.*ich) so channels do not overlap
-      //inTree->Draw("waveform[0][]:Iteration$","Entry$==0");
       if( (ich % chPad64) == 0 ){
-	inTree->Draw(cmd1, cmd2, "goff");
 	mgr = new TMultiGraph( Form("ev%ich%i_%i", event, ich, ich + chPad64 -1),
 			       Form("Event %i channels %i-%i; Time (#times 500 ns); ADC + 100 #times channel #", event, ich, ich + chPad64 -1) );
       }
-      else inTree->Draw(cmd1, cmd2, "samegoff");
-      // Capture output of TTree::Draw
-      if( inTree->GetSelectedRows() ){
-	TGraph *graph = new TGraph( inTree->GetSelectedRows(), inTree->GetV2(), inTree->GetV1() );
+      size_t ichsize = (*waveform)[ich].size();
+      std::vector<int> time(ichsize);
+      std::vector<int> wf(ichsize); // Because TGraph does not accept uint16_t
+      std::vector<int> wfShifted(ichsize); // Shifted copy of the waveform so channels do not overlap
+      if( ichsize > 0 ){
+	// Fill the time vector
+	for(size_t t = 0; t < ichsize; t++){
+	  wf[t] = (int)(*waveform)[ich][t];
+	  wfShifted[t] = wf[t] + ich*100; // Add offset (100.*ich) so channels do not overlap
+	  time[t] = (int)t;
+	}
+	TGraph *graph = new TGraph( wfShifted.size(), &(time[0]), &(wfShifted[0]) );
 	graph->SetMarkerColor(ich % 8 + 1); // Use ROOT colors 1 - 8
 	graph->SetLineColor(ich % 8 + 1);
 	graph->SetNameTitle(Form("ch%i", ich), Form("Channel %i", ich));
 	mgr->Add(graph);
-      }
-      if( ((ich + 1) % chPad64) == 0 ) mgr->Draw("APL");
-      //canvas64->Update();
 
-      // Subset of channels in canvas
-      size_t canvasIndex = (size_t)floor(ich/(colPad16 * rowPad16));
-      //vcanvas16.at( canvasIndex )->cd( colPad16*(ich%rowPad16) + (int)floor(ich/rowPad16) + 1 ); // Plot 64 channels in columns first
-      vcanvas16.at( canvasIndex )->cd( ich%(colPad16 * rowPad16) + 1 );
-      sprintf(cmd1, "waveform[%i][]:Iteration$", ich);
-      if( (ich % chPad16) == 0 ){
-	inTree->Draw(cmd1, cmd2, "goff");
-      }
-      else inTree->Draw(cmd1, cmd2, "samegoff");
-      // Capture output of TTree::Draw
-      if( inTree->GetSelectedRows() ){
-	//TGraph *graph2 = new TGraph( inTree->GetSelectedRows(), inTree->GetV2(), inTree->GetV1() );
-	vgr2.push_back( new TGraph( inTree->GetSelectedRows(), inTree->GetV2(), inTree->GetV1() ) );
+	// Subset of channels in canvas
+	size_t canvasIndex = (size_t)floor(ich/(colPad16 * rowPad16));
+	//vcanvas16.at( canvasIndex )->cd( colPad16*(ich%rowPad16) + (int)floor(ich/rowPad16) + 1 ); // Plot 64 channels in columns first
+	vcanvas16.at( canvasIndex )->cd( ich%(colPad16 * rowPad16) + 1 );
+	vgr2.push_back( new TGraph( wf.size(), &(time[0]), &(wf[0]) ) );
 	TGraph* graph2 = vgr2.back();
 	graph2->SetMarkerColor(ich % 8 + 1); // Use ROOT colors 1 - 8
 	graph2->SetLineColor(ich % 8 + 1);
 	graph2->SetNameTitle(Form("ch%i", ich), Form("Event %i Channel %i; Time (#times 500 ns); ADC", event, ich));
 	graph2->Draw("APL");
-      //vcanvas16.at( canvasIndex )->Update();
+	//vcanvas16.at( canvasIndex )->Update();
+      }
+      if( ((ich + 1) % chPad64) == 0 ){
+	canvas64->cd( (int)floor(ich/chPad64) + 1 ); // Plot "chPad64" channels per pad
+	mgr->Draw("APL");
+	//canvas64->Update();
       }
     }
     canvas64->Update();
